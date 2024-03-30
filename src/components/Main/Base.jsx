5fa25edcdf088b246c5b6de9e7f3399d70e3fd44
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-03-30 18:46:50 -0500

    Gemini UI

--- src/components/Main/Base.jsx
+++ src/components/Main/Base.jsx
@@ -32,6 +32,19 @@ const Base = () => {
                   <img src={assets.code_icon} alt="" />
               </div>
           </div>
+          <div className="main-bottom">
+            <div className="search-box">
+              <input type="text" placeholder='Enter a prompt here...' />
+              <div>
+                <img src={assets.gallery_icon} alt="" />
+                <img src={assets.mic_icon} alt="" />
+                <img src={assets.send_icon} alt="" />
+              </div>
+            </div>
+            <p className="bottom-info">
+            Gemini can make mistakes. Consider checking important information.
+            </p>
+          </div>
         </div>
       </div>
     )
